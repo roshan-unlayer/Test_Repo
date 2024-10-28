97298da7083869d32016f38afd5c300d082708bd
roshan-unlayer <roshan.aziz@unlayer.com>
2024-10-28 23:11:08 +0500

    Create testReact.js

---
+++ testReact.js
@@ -0,0 +1,27 @@
+const Viewer = () => {
+  return <div>I am a custom tool.</div>;
+};
+
+unlayer.registerTool({
+  name: 'my_tool',
+  label: 'My Tool',
+  icon: 'fa-smile',
+  supportedDisplayModes: ['web', 'email'],
+  options: {},
+  values: {},
+  renderer: {
+    Viewer: Viewer, // our React Viewer
+    exporters: {
+      web: function (values) {
+        return '<div>I am a custom tool.</div>';
+      },
+      email: function (values) {
+        return '<div>I am a custom tool.</div>';
+      },
+    },
+    head: {
+      css: function (values) {},
+      js: function (values) {},
+    },
+  },
+});
