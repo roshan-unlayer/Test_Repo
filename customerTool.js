f71f6d0fe4037995e4d5fb2691d2c487753f769c
roshan-unlayer <roshan.aziz@unlayer.com>
2024-10-31 00:59:43 +0500

    Update customerTool.js

--- customerTool.js
+++ customerTool.js
@@ -51,3 +51,33 @@ unlayer.registerTool({
     return []
   },
 })
+
+import React from 'react'
+
+const MyColorPicker = (props) => {
+  const { label, value, updateValue, data } = props
+
+  return (
+    <div>
+      <input
+        class="color-value"
+        defaultValue={value}
+        onChange={(e) => updateValue(e.target.value)}
+      />
+      <button class="red" onClick={() => updateValue('#f00')}>
+        Red
+      </button>
+      <button class="green" onClick={() => updateValue('#0f0')}>
+        Green
+      </button>
+      <button class="blue" onClick={() => updateValue('#00f')}>
+        Blue
+      </button>
+    </div>
+  )
+}
+
+unlayer.registerPropertyEditor({
+  name: 'my_color_picker',
+  Widget: MyColorPicker,
+})
